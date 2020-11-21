0e60b79add4464f4d5d1288b4df9b238bc4e3d04
Hurst <304090717@qq.com>
2020-11-21 21:14:50 +0800

    增加了css-loader

--- 5.Vue/1.webpack/1.webpack的起步/src/main.js
+++ 5.Vue/1.webpack/1.webpack的起步/src/main.js
@@ -1,9 +1,12 @@
-const {add, mul}  = require('./mathUtils.js')
+const {add, mul}  = require('./js/mathUtils.js')
 
 console.log(add(20,30))
 console.log(mul(20,30))
 
 
-import {name, age, height} from './info'
+import {name, age, height} from './js/info'
 
 console.log(name)
+
+//依赖css文件
+require('./css/normal.css')

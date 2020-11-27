046cfc1c1893b270742c10fb198bb72b760ff1cb
Hurst <304090717@qq.com>
2020-11-27 23:31:15 +0800

    冒泡排序

---
+++ 3.JS/3.算法/2.排序算法/1.冒泡排序.js
@@ -0,0 +1,18 @@
+// 由小到大
+function sort(arr) {
+    var len = arr.length
+    var tmp;
+    for(var i=0; i<len-1; i++){
+        for(var j=0; j<len-i-1; j++){
+            if(arr[j]>arr[j+1]) {
+                tmp = arr[j+1]
+                arr[j+1] = arr[j]
+                arr[j] = tmp 
+            }
+        }
+    }
+}
+
+var arr = [12,32,1,3,5,6,7,98,4,3,44]
+sort(arr)
+console.log(arr)

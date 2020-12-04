6fea494c47ca6e7d1274b5664e37ba325e437217
Hurst <304090717@qq.com>
2020-12-04 23:34:33 +0800

    some code

---
+++ 3.JS/5.node.js/web模块/client.js
@@ -0,0 +1,16 @@
+const http = require('http')
+
+const options = {
+    host: 'localhost',
+    port: '8080',
+    path: '/index.html'
+}
+
+const callback = response => {
+    let body = ''
+    response.on('data', data => body += data)
+    response.on('end', () => console.log(body))
+}
+
+const req = http.request(options, callback)
+req.end()
